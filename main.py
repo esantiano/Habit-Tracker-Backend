cdc5b3683f699c028087900524e59f5326ed0dce
esantiano <103700795+esantiano@users.noreply.github.com>
2026-01-09 01:19:49 -0500

    Add CORS middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, habits, dashboard
 from app import models
 from app.db import engine
@@ -17,6 +18,14 @@ app = FastAPI(
 app.include_router(auth.router)
 app.include_router(habits.router)
 app.include_router(dashboard.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost::5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 # this is just a default route and can be removed later 
 @app.get("/")
 def hello():
