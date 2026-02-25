e88d056189159852b8b056873867f0d2d544b919
esantiano <103700795+esantiano@users.noreply.github.com>
2026-02-25 18:31:31 -0500

    Allow CORS from vercel

--- main.py
+++ main.py
@@ -22,7 +22,10 @@ app.include_router(stats.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "https://habit-tracker-rho-topaz.vercel.app",
+        "http://localhost:5173"
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
