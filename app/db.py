f7e3e66f7eabeafb8d3aef5bed922ea64e4bf86f
esantiano <103700795+esantiano@users.noreply.github.com>
2026-02-25 22:48:43 -0500

    database url

--- app/db.py
+++ app/db.py
@@ -4,17 +4,19 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL", 'sqlite:///Habit-Tracker.db')
 
-# if DATABASE_URL.startswith("sqlite"):
-#     engine = create_engine(
-#         DATABASE_URL,
-#         connect_args={"check_same_thread":False},
-#     )
-# else:
-#     engine = create_engine(
-#         DATABASE_URL,
-#         pool_pre_ping=True,
-#     )
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://",1)
+    
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread":False},
+    )
+else:
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True,
+    )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
